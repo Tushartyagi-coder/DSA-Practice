a32848f64a4996c26c40d4965dc642a38b9878f5
Tushartyagi-coder <tushar22092008@gmail.com>
2026-07-08 00:46:28 +0530

    solving of max and min in array

---
+++ Arrays/MinandMaxinArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+pair<int, int> minmaxarray(vector<int>& arr, int n){
+    int maxi = INT_MIN;
+    int mini = INT_MAX;
+    for( int i = 0;i<n;i++){
+        if(arr[i] > maxi){
+            maxi = arr[i];
+        }
+        if(arr[i] < mini){
+            mini = arr[i];
+        }
+    }
+    return {mini , maxi};
+}
+int main(){
+    vector<int> arr = {4 , 6 , 2 , 8 , 1};
+    pair<int, int> ans = minmaxarray(arr , 5);
+    cout << "Minimum element is: " << ans.first << endl;
+    cout << "Maximum element is: " << ans.second << endl;
+    return 0;
+}
