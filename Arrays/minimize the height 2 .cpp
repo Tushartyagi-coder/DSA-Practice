03efa49647a0af991276656522260fe4195c5397
Tushartyagi-coder <tushar22092008@gmail.com>
2026-07-11 22:34:25 +0530

    mi nimize the height

---
+++ Arrays/minimize the height 2 .cpp
@@ -0,0 +1,35 @@
+// minimise the height 2 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int MinimizetheHeight(vector<int> &arr ,  int k){
+    sort(arr.begin() , arr.end());
+    int n = arr.size();
+    int ans = arr[n-1] - arr[0];
+    int smallest = arr[0] + k;
+    int largest = arr[n-1] - k;
+    for( int i = 0 ; i < n- 1; i++){
+        int mini = min(smallest , arr[i+1] - k);
+        int maxi = max(largest , arr[i] + k);
+        ans = min(ans , maxi - mini);
+    }
+    return ans;
+}
+int main(){
+    int n , k;
+    cout << " enter the size of an array:" << endl;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << " enter the value of k:" << endl;
+    cin >> k;
+    cout << " the minimum height is: " << MinimizetheHeight(arr, k) << endl;
+    return 0;
+}
+// this approach is also known as greedy approach with tc O(nlogn) and sc O(1).
+// this approach is also done by sorting the array and then finding the minimum and maximum height after adding and subtracting k from each element.
+//  by name of smallest range1 , 2  on leetcode ;
