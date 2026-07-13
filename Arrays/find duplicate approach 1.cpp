d5a6ff00096c44b8a5758a0ebba45fdefd433d5f
Tushartyagi-coder <tushar22092008@gmail.com>
2026-07-13 23:10:54 +0530

    find duplicate with both approach:

---
+++ Arrays/find duplicate approach 1.cpp
@@ -0,0 +1,52 @@
+// // approach 1: sort the array and check for adjacent elements
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+// int findduplicate(vector<int>& arr){
+//     int n = arr.size();
+//     sort(arr.begin(), arr.end());
+//     for(int i = 0; i < n ; i++){
+//         if((arr[i] ^ arr[i + 1]) == 0){
+//             return arr[i];
+//         }
+//     }
+//     return -1;
+// }
+// approach 2: using floyd's cycle detection to find duplicate
+int findduplicate(vector<int>& arr){
+    int slow = arr[0];
+    int fast = arr[0];
+    do{
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+
+    } while( slow != fast);
+    fast = arr[0];
+    while( slow != fast){
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+    return slow;
+}
+int main(){
+    int n;
+    cout << " enter the size of an array:" << endl;
+    cin >> n;
+    vector<int>arr(n);
+    cout << " enter the elements of an array:" << endl;
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    int result = findduplicate(arr);
+    if(result != -1){
+        cout << " the duplicate element is: " << result << endl;
+    } else {
+        cout << " no duplicate element found." << endl;
+    }
+    return 0;
+
+}
+
+// // this approach has time complexity of O(nlogn) due to sorting and space complexity of O(1) if we sort in place.
+
