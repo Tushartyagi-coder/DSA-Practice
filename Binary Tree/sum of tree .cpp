e765cabc6b4ce4c43dc967e42ae58be72bef6593
Tushartyagi-coder <tushar22092008@gmail.com>
2026-08-30 01:21:17 +0530

    add sum tree validation using pair-based DFS:

---
+++ Binary Tree/sum of tree .cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    node* left;
+    node* right;
+    // constructor 
+    node(int value){
+        data = value;
+        left = right = nullptr;
+    }
+};
+pair<bool , int> sumoftreefast(node* root){
+    if( root == nullptr){
+        return {true , 0};
+
+    }
+    if( root -> left == nullptr && root -> right == nullptr){
+        return {true , root -> data};
+    }
+
+    pair<bool , int> leftans = sumoftreefast(root -> left);
+    pair<bool , int> rightans = sumoftreefast(root -> right);
+    bool left = leftans.first;
+    bool right = rightans.first;
+    bool sum = root -> data == leftans.second + rightans.second;
+    if( left && right && sum){
+        return {true , root -> data + leftans.second + rightans.second};
+    }
+    else{
+        return {false , 0};
+    }
+    
+}
+int main(){
+    node* root  = new node(26);
+    root->left  = new node(10);
+    root->right = new node(3);
+    root->left->left  = new node(4); 
+    root->left->right = new node(6);
+    root->right->left  = new node(3); 
+
+    pair<bool , int> result = sumoftreefast(root);
+
+    cout << "Is Sum Tree : " << (result.first ? "Yes" : "No") << endl; // Yes
+    cout << "Sum of Tree : " << root -> data << endl; // 26
+
+    return 0;
+}
